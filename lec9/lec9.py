dc32c4592f3b3b1446fe22b25d09603ea440fccb
Jasmine Chhikara <jasminechhikara@Jasmines-MacBook-Air.local>
2022-02-03 18:55:29 +0530

    lec9

---
+++ lec9/lec9.py
@@ -0,0 +1,19 @@
+print("Enter the interval")
+a=float(input("Lower limit : "))
+b=float(input("Upper limit : "))
+N=input("Enter the number of intervals : ")
+h=(b-a)/float(N)
+
+def f(x):
+        return(10-x**2)
+A1=0
+A2=0
+
+for i in range(1,int(N)):
+        x=a+i*h
+        if i%2==0:
+                A1=A1+(2*f(x)*h)
+        else:
+                A2=A2+(4*f(x)*h)
+A=((h/3)*(f(a)+f(b)))+((A1+A2)/3)
+print('Integartion value is : ',A)
